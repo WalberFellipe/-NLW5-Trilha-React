4f2740029e12a90fd2049e072f0e04b6bccb161e
Walber Fellipe <walberfellipe18@gmail.com>
2021-04-19 14:25:54 -0300

    first commit

---
+++ Trilha React/podcastr/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./button";
+
+function App() {
+  return (
+    <>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+    </>
+  );
+}
+
+export default App;
